4ee2b917f4f71c81bb9a00d1e35c5675d8f9de6b
Fasamii <fasami0512@gmail.com>
2025-08-26 01:28:13 +0200

    removed write_char functino and created write_chars instead & created read_chars function & started using collections::HashMap for storing chars and accuracy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::{self, HashMap};
+
 mod plane;
 mod store;
 mod terminal;
@@ -5,6 +7,17 @@ mod text;
 
 fn main() {
     let config_path = store::files::get_config_dir().unwrap();
-    println!("config dir : {:?}", config_path);
-    let _ = store::files::write_char(&vec![('a', 123), ('b', 111), ('c', 89)], config_path);
+    println!("config dir : {:?}", &config_path);
+    let _ = store::files::write_chars(vec![('a', 123), ('b', 111), ('c', 89)], &config_path);
+    let mut kp: HashMap<char, i8> = std::collections::HashMap::new();
+    kp.insert('a', 12);
+    kp.insert('b', 12);
+    kp.insert('c', 14);
+    kp.insert('g', 14);
+
+    let _ = store::files::write_chars(kp, &config_path);
+
+    let mut chars_data = store::files::read_chars(&config_path).unwrap();
+    chars_data.shrink_to_fit();
+    println!("chars data is : {chars_data:?}");
 }
--- src/store/files.rs
+++ src/store/files.rs
@@ -45,7 +45,17 @@ pub fn get_config_dir() -> io::Result<path::PathBuf> {
     }))
 }
 
-pub fn write_char(new_records: &Vec<(char, i8)>, config: path::PathBuf) -> csv::Result<()> {
+// STUDY: that function so you can understand it better, you literally struggled to write that and
+// used a lot of code from the internet, make yourself able to write that completely by yourself,
+// todo list to achieve that:
+// - Read the IntoIterator trait docs - to expand your knowledge about traits
+// - Read entire std collections docs - to improve you data structures skills
+// - Write some toy but doing something useful code for each collection - to consolidate
+// PS. Future me plz do that.
+pub fn write_chars<I>(new_records: I, config: &path::PathBuf) -> csv::Result<()>
+where
+    I: IntoIterator<Item = (char, i8)>,
+{
     let file = config.join(CHARS_FILE_NAME);
 
     let mut char_data: collections::HashMap<char, i8> = collections::HashMap::new();
@@ -60,7 +70,7 @@ pub fn write_char(new_records: &Vec<(char, i8)>, config: path::PathBuf) -> csv::
 
             if record.len() != 2 {
                 // TODO: make csv fix function that will fix the csv if its integrity is broken
-                todo!("");
+                todo!("Handle malformed CSV records");
             }
 
             if let (Some(char_str), Some(acc_str)) = (record.get(0), record.get(1)) {
@@ -75,7 +85,7 @@ pub fn write_char(new_records: &Vec<(char, i8)>, config: path::PathBuf) -> csv::
         }
     }
 
-    for &(ch, accuracy) in new_records {
+    for (ch, accuracy) in new_records {
         char_data.insert(ch, accuracy);
     }
 
@@ -86,4 +96,26 @@ pub fn write_char(new_records: &Vec<(char, i8)>, config: path::PathBuf) -> csv::
 
     writer.flush()?;
     Ok(())
+}
+
+pub fn read_chars(config: &path::PathBuf) -> csv::Result<collections::HashMap<char, i8>> {
+    let file = config.join(CHARS_FILE_NAME);
+
+    let mut char_data = collections::HashMap::new();
+    let mut reader = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_path(file)?;
+
+    for record in reader.records() {
+        let record = record?;
+        if let (Some(ch), Some(acc)) = (record.get(0), record.get(1)) {
+            if let Some(parsed_ch) = ch.chars().next() {
+                if let Ok(parsed_acc) = acc.parse::<i8>() {
+                    char_data.insert(parsed_ch, parsed_acc);
+                }
+            }
+        }
+    }
+
+    Ok(char_data)
 }
